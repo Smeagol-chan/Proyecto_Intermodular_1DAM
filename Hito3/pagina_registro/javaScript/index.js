9d1277852926e4d0f619643182ab535924e1d6ba
Denis Rico Renz <>
2026-05-16 00:58:27 +0200

    sombras de las habitaciones, enlaces a la pagina de index corregidos.

---
+++ Hito3/pagina_registro/javaScript/index.js
@@ -0,0 +1,40 @@
+let datosGuardados = localStorage.getItem('usuario');
+let usuario = JSON.parse(datosGuardados);
+const ListaHabitaciones = document.querySelectorAll(".habitaciones");
+let perfil = document.getElementById("perfil");
+let registro = document.getElementById("registro");
+
+if (usuario) {
+    perfil.innerHTML = `${usuario.nombre}`;
+    registro.innerHTML = "Cerrar sesión"
+
+    registro.addEventListener("click", () => {
+        localStorage.removeItem('usuario');
+        window.location.href = "login.html";
+    });
+
+} else {
+    perfil.innerHTML = "Iniciar sesión"
+    registro.innerHTML = "Registrarse"
+
+    perfil.addEventListener("click", () => {
+        window.location.href = "login.html"
+    })
+
+    registro.addEventListener("click", () => {
+        localStorage.removeItem('usuario');
+        window.location.href = "registro.html";
+    });
+
+}
+
+//El boton del perfil manda al perfil de inquilino o de arrendador dependiendo el tipo de usuario
+let inq = usuario.tipo;
+
+perfil.addEventListener("click", () => {
+    if (inq === "Inquilino") {
+        window.location.href = "perfil-inquilino.html";
+    } else if (inq === "Arrendador") {
+        window.location.href = "perfil-arrendador.html";
+    }
+})
